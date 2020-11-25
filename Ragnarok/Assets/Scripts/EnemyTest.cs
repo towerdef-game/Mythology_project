d9c17d4e7252fe53e51419bee23fa821896704d4
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-25 18:13:24 +0000

    have attack animations done for heimdall and odin, they kill the eney when they do enough damage

--- Ragnarok/Assets/Scripts/EnemyTest.cs
+++ Ragnarok/Assets/Scripts/EnemyTest.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class EnemyTest : MonoBehaviour
 {
-    public Player player; 
+   // public Player player; 
+    public int maxhealth = 100;
+    public int currentHealth; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxhealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentHealth<= 0)
+        {
+            Die();
+        }
     }
 
    public  void OnTriggerEnter()
     {
         
+    }
+    public void TakeDamage(int damage)
+    {
+
+        currentHealth -= damage;
+       // playerHealthBar.SetHealth(currentPlayerHealth);
+
+    }
+    public void Die()
+    {
+         //player.GetComponent<Player>().KillCount(3);
+        // player.KillCount(3);
+        Destroy(this.gameObject,0.1f);
     }
 }

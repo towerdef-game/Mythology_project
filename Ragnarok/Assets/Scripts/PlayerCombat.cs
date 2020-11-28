cea5ded591ff93a6ce152313fa5413aff9b53852
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-28 17:33:44 +0000

    hope please

--- Ragnarok/Assets/Scripts/PlayerCombat.cs
+++ Ragnarok/Assets/Scripts/PlayerCombat.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerCombat : MonoBehaviour
 {
@@ -8,12 +10,22 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask Enemy;
-
     public float attackRate = 2f;
     float nextAttack = 0;
-   // public EnemyTest enemy;
     public int damage = 10;
+    public int currentPlayerKill;
+    public int poweUp = 2;
+    public TextMeshProUGUI PowerText;
+    public TextMeshProUGUI PowerTextRemaining;
+    public TextMeshProUGUI PowerTextRemainingTimer;
+    public float timer = 3;
    
+   public void Start()
+   {
+     //PowerTextRemainingTimer.text = timer.t
+     PowerTextRemainingTimer.SetText(timer.ToString());
+   }
+
     void Update()
     {
         if(Time.time >= nextAttack)
@@ -24,8 +36,36 @@ public class PlayerCombat : MonoBehaviour
                 nextAttack =  Time.time + 1f/attackRate;
             }
         }
+        if (Input.GetKeyDown (KeyCode.Mouse1)) 
+            {
+            //KillCount(1);
+            currentPlayerKill ++;
+            }
+
+        if(currentPlayerKill >= 5)
+        {         
+            PowerText.enabled = true;
+           
+
+            if (Input.GetKeyDown (KeyCode.Z)) 
+           {
+                PowerText.enabled = false;
+                PowerTextRemaining.enabled = true;
+                PowerTextRemainingTimer.enabled = true;
+                timer -= Time.deltaTime;
+                StartCoroutine(("PowerUp"));
+                currentPlayerKill -= 5;
+                if(timer < 0)
+                {
+                    PowerTextRemaining.enabled = false;
+                    PowerTextRemainingTimer.enabled = false;     
+                }
+           
+            }
+        }
     }
-    public void Attack()
+
+     void Attack()
     {
         animator.SetTrigger("Attack");
         Collider2D[] hit = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, Enemy);
@@ -43,4 +83,17 @@ public class PlayerCombat : MonoBehaviour
         return;
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
+
+    IEnumerator  PowerUp()
+    {
+         damage *= poweUp;
+         attackRange *= poweUp;
+         yield return new WaitForSeconds(3);
+          damage /= poweUp;
+         attackRange /= poweUp;
+    }
+
+
 }
+
+

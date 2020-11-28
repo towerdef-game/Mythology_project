dc22e461aed0d2b38f8a197f36fed33eb1e5449d
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-28 14:39:01 +0000

    camera

---
+++ Ragnarok/Assets/Scripts/camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x = player.position.x;
+       temp.y = player.position.y;
+
+        transform.position = temp;
+    }
+}

591d8622e0ffca470ef461ebc1d9ab5650065308
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-28 14:07:40 +0000

    HOPE 6

--- Ragnarok/Assets/Scripts/CamFollow.cs
+++ Ragnarok/Assets/Scripts/CamFollow.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamFollow : MonoBehaviour {
-	public Transform player;
+	private GameObject Player;
+	private GameObject Player2;
+	private GameObject Player3;
+	  private Vector3 offset;
 
-	// Update is called once per frame
-	void Update () {
-		transform.position = new Vector3 (player.position.x, player.position.y, transform.position.z);
+    void Start()
+    {
+        Player = GameObject.Find("/Player 1/CharacterList/ss_thor_idle_0");
+		 Player2 = GameObject.Find("/Player 1/CharacterList/ss_odin_idle_0");
+		  Player3 = GameObject.Find("/Player 1/CharacterList/ss_heim_idle_0");
 		
-	}
+		   offset = transform.position - Player.transform.position;
+		   offset = transform.position - Player2.transform.position;
+		   offset = transform.position - Player3.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Player.transform.position + offset;
+		 transform.position = Player2.transform.position + offset;
+		  transform.position = Player3.transform.position + offset;
+    }
 }

7e1385363a37eb7f73508043a4d58dfa6eda45f8
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-29 18:17:03 +0000

    music 2

--- Ragnarok/Assets/Scripts/Music.cs
+++ Ragnarok/Assets/Scripts/Music.cs
@@ -7,7 +7,16 @@ public class Music : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         DontDestroyOnLoad(this.gameObject);
+         int music = FindObjectsOfType<Music>().Length;
+        if (music > 1)
+        {
+            Destroy(gameObject);// if there is more than 1  GameManager scripts in the game destroy this one
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
     }
 
     // Update is called once per frame

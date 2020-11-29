7e936ddb3681b59b1a24b330ca3cabaf88566cd4
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-29 18:08:12 +0000

    portal and endgame

---
+++ Ragnarok/Assets/endgame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endgame : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetBool("end", true);
+        }
+    }
+
+    void nextscene()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}

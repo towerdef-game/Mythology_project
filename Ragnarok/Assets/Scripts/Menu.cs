e2947681b46c07bc154c0f0b68b33f566773c5ad
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-29 13:10:01 +0000

    **_**

---
+++ Ragnarok/Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+
+
+
+    public void PlayGamelevel()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+        //SceneFader.FadTo("Loading");
+    }
+
+ 
+
+    public void GameMenuControls()
+    {
+        SceneManager.LoadScene("Controls");
+      
+    }
+
+
+    public void QuitGameMenu()
+    {
+
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        //SceneFader.FadTo("Menu");
+    }
+}

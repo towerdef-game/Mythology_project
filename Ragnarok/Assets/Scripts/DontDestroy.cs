4cdab1efc0add8a3ce759cf625b5f80e398e62df
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-12-12 19:38:34 +0000

    patrol points

---
+++ Ragnarok/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         int numbGameManagers = FindObjectsOfType<GameManager>().Length;
+        if (numbGameManagers > 1)
+        {
+            Destroy(gameObject);// if there is more than 1  GameManager scripts in the game destroy this one
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

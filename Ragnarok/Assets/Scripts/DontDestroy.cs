f2dfc0f4b52f9e69eb172e5ea20e684f990c1228
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-12-12 19:50:27 +0000

    portal working lv 2

--- Ragnarok/Assets/Scripts/DontDestroy.cs
+++ Ragnarok/Assets/Scripts/DontDestroy.cs
@@ -1,27 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
+   
+    public Animator anim;
+    // Start is called before the first frame update
     void Start()
     {
-         int numbGameManagers = FindObjectsOfType<GameManager>().Length;
-        if (numbGameManagers > 1)
-        {
-            Destroy(gameObject);// if there is more than 1  GameManager scripts in the game destroy this one
-        }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+       
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetBool("end", true);
+        }
+    }
+
+    void nextscene()
+    {
+        SceneManager.LoadScene("Win");
     }
 }

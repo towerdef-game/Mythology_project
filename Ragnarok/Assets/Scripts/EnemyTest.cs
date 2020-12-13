6a69c818eeaa974abbdf27c0340185e30deff98f
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-12-13 14:15:17 +0000

    enemy damage

--- Ragnarok/Assets/Scripts/EnemyTest.cs
+++ Ragnarok/Assets/Scripts/EnemyTest.cs
@@ -10,15 +10,26 @@ public class EnemyTest : MonoBehaviour
     public int currentHealh;
     // Start is called before the first frame update
     public Animator anim;
+<<<<<<< Updated upstream
     public AudioClip clips;
     public AudioSource source;
+=======
+    public SpriteRenderer sr;
+    private Material red;
+    private Material matdefault;
+>>>>>>> Stashed changes
     void Start()
     {
         currentHealh = maxHealth;
 
         player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponentInParent<Animator>();
+<<<<<<< Updated upstream
         source = GetComponent<AudioSource>();
+=======
+        red = Resources.Load("red", typeof(Material)) as Material;
+        matdefault = sr.material;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -28,10 +39,16 @@ public class EnemyTest : MonoBehaviour
         {
      
             die();
+        } else
+        {
+           
         }
     }
-
-   public void die()
+    void resetmaterial()
+    {
+        sr.material = matdefault;
+    }
+    public void die()
     {
 
         player = GameObject.FindGameObjectWithTag("Player");
@@ -41,10 +58,11 @@ public class EnemyTest : MonoBehaviour
     }
     public void TakeDamageEnemy(int damage)
     {
-
+        sr.material = red;
         currentHealh -= damage;
         // playerHealthBar.SetHealth(currentPlayerHealth);
-     anim.SetBool("attacked", true);
+        // anim.SetBool("attacked", true);
+        Invoke("resetmaterial", .5f);
     }
     public void resetdamge()
     {

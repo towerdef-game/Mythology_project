31d734008b88545de06943d261c9c8b1ab60f0f9
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-12-13 14:18:12 +0000

    Revert "Merge branch 'coding-john' of https://github.com/towerdef-game/Mythology_project into coding-john"
    This reverts commit b5bbc7337768fe0bd9db74d58237dd6971c7288d, reversing
    changes made to a9ae54a98fe196cac0286fa8bc7acdf1cf51e913.

--- Ragnarok/Assets/Scripts/EnemyTest.cs
+++ Ragnarok/Assets/Scripts/EnemyTest.cs
@@ -10,59 +10,42 @@ public class EnemyTest : MonoBehaviour
     public int currentHealh;
     // Start is called before the first frame update
     public Animator anim;
-<<<<<<< Updated upstream
     public AudioClip clips;
     public AudioSource source;
-=======
-    public SpriteRenderer sr;
-    private Material red;
-    private Material matdefault;
->>>>>>> Stashed changes
+    public bool dead = false;
     void Start()
     {
         currentHealh = maxHealth;
 
         player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponentInParent<Animator>();
-<<<<<<< Updated upstream
         source = GetComponent<AudioSource>();
-=======
-        red = Resources.Load("red", typeof(Material)) as Material;
-        matdefault = sr.material;
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentHealh <=0)
+        if(currentHealh <=0 && !false)
         {
-     
+            dead = true;
             die();
-        } else
-        {
-           
         }
     }
-    void resetmaterial()
-    {
-        sr.material = matdefault;
-    }
-    public void die()
+
+   public void die()
     {
 
         player = GameObject.FindGameObjectWithTag("Player");
         source.Play();
         player.GetComponent<PlayerCombat>().currentPlayerKill++;
-          Destroy(thisobject,2f);
+          Destroy(thisobject);
     }
     public void TakeDamageEnemy(int damage)
     {
-        sr.material = red;
+
         currentHealh -= damage;
         // playerHealthBar.SetHealth(currentPlayerHealth);
-        // anim.SetBool("attacked", true);
-        Invoke("resetmaterial", .5f);
+     anim.SetBool("attacked", true);
     }
     public void resetdamge()
     {
